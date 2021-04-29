92c37d0b43ac229260349b8ae62f9e5b58f61747
baradweelrawan <bardaweel_rawan@yahoo.com>
2021-04-29 12:39:18 +0300

    solve problem1

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a, b) {
   //eslint-disable-line
+  let sum = a +b ;
+  return [sum , `The sum of ${a} and ${b} is in ${sum}`];
 }
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
